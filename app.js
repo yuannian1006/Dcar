74ab603d8930b84a1c33edd4e523d905fd9bd61b
yuannian1006 <32972980+yuannian1006@users.noreply.github.com>
2018-09-14 20:11:44 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -9,37 +9,53 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+
+
+
+
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        if (res.code) {
-          //发起网络请求
-          wx.request({
-            url: 'http://localhost:8188/xxl-auth/system/user/login',
-            data: {
-              code: res.code
-            },
-            success: function (data) {
-              console.log(data);
-            },
-            fail: function (res) {
-              console.log(res)
-            }
-          })
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      }
-    })
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //     if (res.code) {
+    //       //发起网络请求
+    //       wx.request({
+    //         url: 'https://jsxsdqc.com:8188/xxl-auth/system/user/login',
+    //         data: {
+    //           code: res.code
+    //         },
+    //         success: function (data) {
+    //           console.log(data);
+    //         },
+    //         fail: function (res) {
+    //           console.log(res)
+
+
+
+    
+    //         }
+    //       })
+    //     } else {
+    //       console.log('获取用户登录态失败！' + res.errMsg)
+    //     }
+    //   }
+    // })
 
   },
 
   globalData: {
+    /*** 开发环境地址  ***/
+    apiUrl: 'https://localhost',
+    /*** 正式环境地址  ***/
+    // apiUrl: 'https://jsxsdqc.com',
+
+    /*** 确证  ***/
     userInfo: null,
     token: null,
     openid: null,
-
+    
+    /*** 用户信息 ***/
+    tsSysUserId:null,
     no: null,
     name: null, //姓名
     email: null,//邮箱
@@ -49,8 +65,12 @@ App({
     userStatus: null,//用户状态
     userType: null,//用户类型
     points: null,//积分
-    sex: null//性别
+    sex: null,//性别
+   
+    /*** 业务场景需要用字段 ***/
+   
+
   },
 
-  debugger: true
+  debugger: false
 })
