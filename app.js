6081d98cb63550bac1e764a0a5082b48d3102598
yn48926 <yn48926@ly.com>
2018-09-14 21:06:53 +0800

    租车小程序

---
+++ app.js
@@ -0,0 +1,76 @@
+//app.js
+App({
+
+
+
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+
+
+
+
+    // 登录
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //     if (res.code) {
+    //       //发起网络请求
+    //       wx.request({
+    //         url: 'https://jsxsdqc.com:8188/xxl-auth/system/user/login',
+    //         data: {
+    //           code: res.code
+    //         },
+    //         success: function (data) {
+    //           console.log(data);
+    //         },
+    //         fail: function (res) {
+    //           console.log(res)
+
+
+
+    
+    //         }
+    //       })
+    //     } else {
+    //       console.log('获取用户登录态失败！' + res.errMsg)
+    //     }
+    //   }
+    // })
+
+  },
+
+  globalData: {
+    /*** 开发环境地址  ***/
+    apiUrl: 'https://localhost',
+    /*** 正式环境地址  ***/
+    // apiUrl: 'https://jsxsdqc.com',
+
+    /*** 确证  ***/
+    userInfo: null,
+    token: null,
+    openid: null,
+    
+    /*** 用户信息 ***/
+    tsSysUserId:null,
+    no: null,
+    name: null, //姓名
+    email: null,//邮箱
+    mobile: null,//手机号
+    status: null,//启用禁用
+    imgUrl: null,//图片地址
+    userStatus: null,//用户状态
+    userType: null,//用户类型
+    points: null,//积分
+    sex: null,//性别
+   
+    /*** 业务场景需要用字段 ***/
+   
+
+  },
+
+  debugger: false
+})
---
+++ pages/home/wallet/wallet.js
@@ -0,0 +1,80 @@
+// pages/wallet/wallet.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+ 
+    grids: [
+      { title: '礼品卡'}, 
+      { title: '企业服务' },
+      { title: '卡包' }, 
+      { title: '付款码' },
+      { title: '收钱' },
+      { title: '流量充值' },
+      { title: '汽车商城' },
+      { title: '手机充值' },
+      { title: '保险商城' },
+      ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+
+
+  
+})
